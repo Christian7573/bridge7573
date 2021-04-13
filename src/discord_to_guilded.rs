20f2accd6f0aea7a8092516a3ae7c196d905cb12
Christian7573 <cag4561@gamil.com>
2021-04-13 09:32:58 -0500

    Beginning work on discord to guilded message passing

--- src/discord_to_guilded.rs
+++ src/discord_to_guilded.rs
@@ -32,6 +32,76 @@ pub(crate) async fn discord_to_guilded(env: Arc<Environment>, mut from_discord: 
     let print_all_msg = std::env::var("print_all_msg").is_ok();
 
     async_std::task::spawn(async move {
+        #[derive(Deserialize)]
+        struct IncomingMsg {
+            op: i64,
+            t: String,
+            d: JsValue
+        }
+
+        while let Some(msg) = from_discord.next().await {
+            if let WsMessage::Text(msg) = &*msg {
+                if let Ok(msg) = deserialize::<IncomingMsg>(&msg) {
+                   if msg.op == 0 {
+                       match &*msg.t {
+                           "MESSAGE_CREATE" => {
+                               if let Ok(msg) = DiscordMessage::deserialize(msg.d) {
+                                   message_created(&env, &mut data, msg).await;
+                               }
+                           }
+                           _ => ()
+                       };
+                   }
+                }
+            }
+        }
 
     })
+}
+
+#[derive(Deserialize)]
+struct DiscordMessage {
+    //id: String,
+    channel_id: String,
+    author: DiscordUser,
+    webhook_id: Option<String>,
+}
+#[derive(Deserialize)]
+struct DiscordUser {
+    id: String,
+    username: String,
+    avatar: Option<String>,
+}
+
+async fn message_created(env: &Arc<Environment>, data: &mut Data, msg: DiscordMessage) {
+    if msg.webhook_id.is_some() { return };
+    let guilded_channel = if let Some(c) = get_linked_guilded_channel(env, data, &msg.channel_id) { c } else { return };
+    let webhook = match get_webhook(env, data, &msg.author, guilded_channel).await {
+        Ok(w) => w, 
+        Err(err) => { eprintln!("GD Chat Message Get Webhook: {:?}", err); return; }
+    };
+    
+}
+
+fn get_linked_guilded_channel<'e>(env: &'e Arc<Environment>, _data: &mut Data, discord_channel: &str) -> Option<&'e str> {
+    env.config.text_channel_dg.get(discord_channel).map(|s| &**s)
+}
+
+async fn get_webhook(env: &Arc<Environment>, data: &mut Data, user: &DiscordUser, guilded_channel: &str) -> Result<String, ErrorBox> {
+    //Get from database
+    if data.webhooks.get(guilded_channel).is_none() { data.webhooks.insert(guilded_channel.to_owned(), BTreeMap::new()); }
+    let channel = data.webhooks.get_mut(guilded_channel).unwrap();
+    if let Some(webhook) = channel.get(&user.id) { return Ok(webhook.to_owned()) };
+
+    let avatar = if let Some(avatar_hash) = user.avatar.as_ref() {
+        let mut response = surf::get(format!("https://cdn.discordapp.com/avatars/{}/{}.png?size=512", user.id, avatar_hash))
+            .send().await?;
+        if !response.status().is_success() { return Err(format!("DG Make Webhook: Failed to get avatar for user {}: {}", user.id, response.status()).into()) };
+        let bytes = response.body_bytes().await?;
+        todo!()
+    } else { None };
+
+
+
+    todo!()
 }
